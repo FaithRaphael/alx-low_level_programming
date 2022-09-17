ab9a4e2b98aea50ff7d413d886543cdeba6c2f0e
FaithRaphael <raphaelfaith1999@gmail.com>
2022-09-17 13:06:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Print all single digit numbers using only putchar
- *
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int d;
+  int n;
 
-  for (d = 0; d < 10; d++)
-    putchar(d + '0');
-
+  for (n = 48; n < 58; n++)
+    {
+      putchar(n);
+    }
   putchar('\n');
-
   return (0);
 }
